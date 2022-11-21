779a1cce75ca9cc150d14a78075524ab347128eb
ReyD9 <92357513+ReyD9@users.noreply.github.com>
2022-11-21 23:51:31 +0530

    Add files via upload
    soon be completed

---
+++ Header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar,Toolbar,Typography,InputBase,Box } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search"
+import useStyles from './styles.js';
+ const Header = () => {
+     const classes = useStyles();
+     return (
+        <AppBar position="static">
+            <Toolbar className={classes.Toolbar}>
+                <Typography variant="h5" className={classes.title}>
+                    Breaks 2 Brakes
+                </Typography>
+                <Box display="flex">
+                    <Typography variant="h6" className={classes.title}>
+                        Explore
+                    </Typography>
+                   {/* <Autocomplete> */}
+                        <div className={classes.search}>
+                            <div className={classes.SearchIcon}>
+                                <SearchIcon/>
+                            </div>
+                                <InputBase placeholder="Search..." classes={{ root:classes.inputRoot, input: classes.inputInput}}/>
+                         </div>
+                    {/*</Autocomplete>*/}
+                </Box>
+            </Toolbar>
+        </AppBar>
+     );
+ }
+
+ export default Header;
